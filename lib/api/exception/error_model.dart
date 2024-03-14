9be3fc4e3a93dca646aaf17bbc2d6b62f0d1ae8e
ankitavaghasiya <vaghasiyaankita213@gmail.com>
2024-03-14 22:13:27 +0530

    # Error handaling api responses

---
+++ lib/api/exception/error_model.dart
@@ -0,0 +1,18 @@
+class ErrorModel {
+  bool? success;
+  String? message;
+
+  ErrorModel({this.success, this.message});
+
+  ErrorModel.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['success'] = this.success;
+    data['message'] = this.message;
+    return data;
+  }
+}
